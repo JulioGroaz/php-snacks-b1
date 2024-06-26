83e062c7b33ba9b88273061b19e38d14436e308f
JulioGroaz <julio.groaz2020@gmail.com>
2024-06-26 12:33:15 +0200

    Aggiunto il fatto che i punteggi delle partite vengano creati in maniera randomica da 0-100.

--- snack1/snack1.php
+++ snack1/snack1.php
@@ -5,30 +5,29 @@ $partite = [
     [
         "squadra_casa" => "Olimpia Milano", 
         "squadra_ospite" => "Cantù", 
-        "punti_casa" => 55, 
-        "punti_ospite" => 60
+
     ],
     [
         "squadra_casa" => "Virtus Bologna", 
         "squadra_ospite" => "Reyer Venezia", 
-        "punti_casa" => 75, 
-        "punti_ospite" => 70
+
     ],
     [
         "squadra_casa" => "Pallacanestro Varese", 
         "squadra_ospite" => "Fortitudo Bologna", 
-        "punti_casa" => 85, 
-        "punti_ospite" => 80
+   
     ],
     [
         "squadra_casa" => "Dinamo Sassari", 
         "squadra_ospite" => "Pallacanestro Trieste", 
-        "punti_casa" => 90, 
-        "punti_ospite" => 85
+
     ],
 ];
-//Stampato in pagina come da consegna ogni elemento dell'array partite che le relative caratteristiche.
+//Stampato in pagina come da consegna ogni elemento dell'array partite che le relative caratteristiche. Aggiunto il fatto che vengano creati i punteggi delle partite in maniera randomica. 
+
 foreach ($partite as $partita) {
+    $partita["punti_casa"] = rand(0, 100);
+    $partita["punti_ospite"] = rand(0, 100);
     echo $partita["squadra_casa"] . " - " . $partita["squadra_ospite"] . " | " . $partita["punti_casa"] . "-" . $partita["punti_ospite"] . "<br>";
 }
 ?>
