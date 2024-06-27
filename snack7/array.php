830384a7141cb7734421bea05ebc165a680bb56b
JulioGroaz <julio.groaz2020@gmail.com>
2024-06-27 12:37:10 +0200

    esercizio 7 completato

---
+++ snack7/array.php
@@ -0,0 +1,25 @@
+
+<?php
+return [
+    [
+        'nome' => 'Mario',
+        'cognome' => 'Rossi',
+        'voti' => [6, 7, 8, 9]
+    ],
+    [
+        'nome' => 'Luca',
+        'cognome' => 'Bianchi',
+        'voti' => [5, 7, 6, 8]
+    ],
+    [
+        'nome' => 'Giulia',
+        'cognome' => 'Verdi',
+        'voti' => [9, 8, 7, 10]
+    ],
+    [
+        'nome' => 'Anna',
+        'cognome' => 'Neri',
+        'voti' => [6, 7, 8, 9]
+    ]
+];
+?>
---
+++ snack7/snack7.php
@@ -0,0 +1,11 @@
+<?php
+$array = __DIR__;
+foreach ($alunni as $alunno) {
+    $nome = $alunno['nome'];
+    $cognome = $alunno['cognome'];
+    $voti = $alunno['voti'];
+    $mediaVoti = array_sum($voti) / count($voti);
+                    
+    echo "Nome: $nome, Cognome: $cognome, Media dei voti: " . number_format($mediaVoti, 2) . "<br>";
+}
+?>
